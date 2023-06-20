65f5bf167a3cb644396110438527a71ac0e372ed
Thiago Gomes <thiago.silgom@gmail.com>
2023-06-20 11:08:28 -0400

    whatsapp/email link

--- templates/Contacts/view.php
+++ templates/Contacts/view.php
@@ -1,5 +1,5 @@
 <h1><?= h($contact->name) ?></h1>
 <?= $this->Html->link('Voltar', ['action' => 'index']) ?>
-<p><strong>Telefone:</strong> <?= h($contact->phone) ?></p>
-<p><strong>E-mail:</strong> <?= h($contact->email) ?></p>
+<p><strong>Telefone:</strong> <?= $this->Html->link($contact->phone, 'https://api.whatsapp.com/send?phone=' . str_replace(['(',')',' ', '-'], '', $contact->phone), ['target' => '_blank']) ?></p>
+<p><strong>E-mail:</strong> <?= $this->Html->link($contact->email, 'mailto:' . $contact->email, ['target' => '_blank']) ?></p>
 <p><small>Data: <?= $contact->created->format('d/m/Y') ?></small></p>
